1971b12d02db04f8c21e8214071d8727ac05ff22
Mozahidulislamnibir <mozahidulislam000@gmail.com>
2021-07-03 22:01:56 +0600

    file uploaded

---
+++ custom.js
@@ -0,0 +1,49 @@
+var a;
+let b;
+const c = 20;
+
+a = 15;
+b = 10;
+
+console.log(
+    'a = ' + a,
+    'c = ' + c,
+    'a+b = ' + (a + b),
+    a + b,
+);
+
+a = 150;
+b = 30;
+console.log(a + b);
+
+a = 5;
+b = 4;
+console.log(a + b);
+console.log(a - b);
+console.log(a / b);
+console.log(a % b);
+
+integer
+let data_type = 100;
+console.log(data_type, typeof data_type);
+
+// float
+data_type = 100.50;
+console.log(data_type, typeof data_type);
+
+// double
+data_type = 100.50324234234543534;
+console.log(data_type, typeof data_type);
+
+// string
+data_type = "my name is someone";
+console.log(data_type, typeof data_type);
+
+data_type = null;
+console.log(data_type, typeof data_type);
+
+data_type = undefined;
+console.log(data_type, typeof data_type);
+
+data_type = ((2 + (5 - 2)) * (2 + 3) / 2);
+console.log(data_type, typeof data_type);
